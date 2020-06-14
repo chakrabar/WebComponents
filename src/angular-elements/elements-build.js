8d02ab90c3c8bb32edc952f033985f3b835c9289
arghya.chakrabarty@uipath.com <arghya.chakrabarty@uipath.com>
2020-06-14 18:22:16 +0530

    Some small fixes to make the the basic WebComponents run end-to-end

--- src/angular-elements/elements-build.js
+++ src/angular-elements/elements-build.js
@@ -7,9 +7,9 @@ const concat = require('concat');
         './dist/angularElements/scripts.js',
         './dist/angularElements/main.js',
     ]
-    await fs.ensureDir('elements')
-    await concat(files, 'elements/test-element03.js');
-    await fs.copyFile('./dist/angularElements/styles.css', 'elements/styles.css')
+    await fs.ensureDir('dist/es5_web_component')
+    await concat(files, 'dist/es5_web_component/test-element03.js');
+    await fs.copyFile('./dist/angularElements/styles.css', 'dist/es5_web_component/styles.css')
     // await fs.copy('./dist/angularElements/assets/', 'elements/assets/' )
     
 })()
